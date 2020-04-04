1356e70ae22ff97d38cef58f65dd8df55c3f078d
Tanya <dz.tatiana.v@gmail.com>
2020-04-04 22:32:12 +0300

    Timer

--- js/script.js
+++ js/script.js
@@ -33,4 +33,58 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    //Timer
+
+    let deadline = '2020-04-20';
+
+    function getTimeRemaining(endTime) {
+        let t = Date.parse(endTime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/1000/60/60));
+
+        return {
+            'total': t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function setClock(id, endTime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endTime);
+
+            if (t.hours < 10) {
+                hours.textContent = '0' + t.hours;
+            } else if (t.minutes < 10) {
+                minutes.textContent = '0' + t.minutes;
+            } else if (t.seconds < 10) {
+                seconds.textContent = '0' + t.seconds;
+            } else {
+                hours.textContent = t.hours;
+                minutes.textContent = t.minutes;
+                seconds.textContent = t.seconds;
+            }
+            // hours.textContent = t.hours;
+            // minutes.textContent = t.minutes;
+            // seconds.textContent = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+
+    setClock('timer', deadline);
 });
